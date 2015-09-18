d528b24cf2e1bdff1ec6526c3131aa5ecc2b5279
ershov-ilya <ershov.ilya@gmail.com>
2015-09-18 16:35:05 +0300

    продление одной подписки

--- cron/event_stop_or_continue.php
+++ cron/event_stop_or_continue.php
@@ -15,26 +15,36 @@ $modx->setLogTarget('FILE');
 $modx->error->message = null;
 
 $payandsee = $modx->getService('payandsee');
-$msprofile = $modx->getService('msprofile');
 
+$time=time();
 $now = date("Y-m-d H:i:s");
+$month = date("Y-m-d H:i:s",$time+2629800);
 
 $active = 1;
 // выбираем активные подписки с просроченной датой окончания
-$q = $modx->newQuery('PaySeeList', array('active' => $active));
-$q->leftJoin('PaySeeResourceSettings','PaySeeResourceSettings','PaySeeResourceSettings.resource_id = PaySeeList.resource_id');
-$q->where(array(
-	'PaySeeList.stopdate:<=' => $now,
-));
-$q->select($modx->getSelectColumns('PaySeeList','PaySeeList'));
-$q->select($modx->getSelectColumns('PaySeeResourceSettings','PaySeeResourceSettings'));
-
-$data = $modx->getIterator('PaySeeList', $q);
 $modx->lexicon->load('payandsee:default');
 
 if(D) {
 	if(isset($_GET['id'])){
 		print "<pre>".PHP_EOL;
+
+        $q = $modx->newQuery('PaySeeList', array(
+            //'active' => $active,
+            'user_id' => 6,
+        ));
+        $q->leftJoin('PaySeeResourceSettings','PaySeeResourceSettings','PaySeeResourceSettings.resource_id = PaySeeList.resource_id');
+        $q->where(array(
+            'PaySeeList.stopdate:>' => 0,
+        ));
+        $q->select($modx->getSelectColumns('PaySeeList','PaySeeList'));
+        $q->select($modx->getSelectColumns('PaySeeResourceSettings','PaySeeResourceSettings'));
+        $data = $modx->getIterator('PaySeeList', $q);
+
+        foreach ($data as $d) {
+            $pas = $d->toArray();
+            print_r($pas);
+        }
+
 		// Профиль покупателя
 		$user_id= $_GET['id'];
 		$user = $modx->getObject('modUser', $user_id);
@@ -44,32 +54,70 @@ if(D) {
 		if (!$msCustomerProfile = $modx->getObject('msCustomerProfile', array('id' => $user_id))) {
 			die('No customer profile');
 		}else{
-			var_dump($msCustomerProfile->toArray());
+            $arrProfile=$msCustomerProfile->toArray();
+			var_dump($arrProfile);
 		}
 		
 		// Стоимость продления
-		
-		die;
+        print "Стоимость продления".PHP_EOL;
+        $cost=$pas['pas_price'];
+        print $cost.PHP_EOL;
+        print "Баланс".PHP_EOL;
+        print $arrProfile['account'].PHP_EOL;
+
+        // смена статуса подписки на продлённую
+        $res=false;
+        if ($objPAS = $modx->getObject('PaySeeList', array(
+            'resource_id' => 16,
+            'user_id' => 6,
+        ))) {
+            $objPAS->fromArray(array(
+                'active' => 1,
+                'stopdate' => $month,
+            ));
+            $res=$objPAS->save();
+        }
+        var_dump($res);
+
+        if($res) {
+            $res=false;
+            // Вычитание баланса
+            $msCustomerProfile->fromArray(array(
+                'account' => $arrProfile['account'] - $cost
+            ));
+            $res=$msCustomerProfile->save();
+        }
+
+        die;
 	}
 
 }
 
+$q = $modx->newQuery('PaySeeList', array('active' => $active));
+$q->leftJoin('PaySeeResourceSettings','PaySeeResourceSettings','PaySeeResourceSettings.resource_id = PaySeeList.resource_id');
+$q->where(array(
+    'PaySeeList.stopdate:<=' => $now,
+));
+$q->select($modx->getSelectColumns('PaySeeList','PaySeeList'));
+$q->select($modx->getSelectColumns('PaySeeResourceSettings','PaySeeResourceSettings'));
+$data = $modx->getIterator('PaySeeList', $q);
+
 
 foreach ($data as $d) {
-	$pls = $d->toArray();
+	$pas = $d->toArray();
 	$subject = '';
 	if ($chunk = $modx->newObject('modChunk', array('snippet' => $modx->lexicon('pas_subject_stopdate')))){
 		$chunk->setCacheable(false);
-		$subject = $payandsee->processTags($chunk->process($pls));
+		$subject = $payandsee->processTags($chunk->process($pas));
 	}
 	$body = 'no chunk set';
 	if ($chunk = $modx->getObject('modChunk', $modx->getOption('payandsee_chunk_stopdate', null, 66))) {
 		$chunk->setCacheable(false);
-		$body = $payandsee->processTags($chunk->process($pls));
+		$body = $payandsee->processTags($chunk->process($pas));
 	}
 	if (!empty($subject)) {
 		
-		$user = $modx->getObject('modUser', $pls['user_id']);
+		$user = $modx->getObject('modUser', $pas['user_id']);
 		$profile=$user->getOne('Profile');
 		
 		$msprofile=$user->getOne('msProfile');
@@ -79,11 +127,11 @@ foreach ($data as $d) {
 		}
 		
 		// письмо пользователю
-		$payandsee->addQueue($pls['user_id'], $subject, $body, '');
+		$payandsee->addQueue($pas['user_id'], $subject, $body, '');
 		// смена статуса подписки на неактивную
 		if ($data_ = $modx->getObject('PaySeeList', array(
-			'resource_id' => $pls['resource_id'],
-			'user_id' => $pls['user_id'],
+			'resource_id' => $pas['resource_id'],
+			'user_id' => $pas['user_id'],
 		))) {
 			$data_->fromArray(array('active' => 0));
 			$data_->save();
